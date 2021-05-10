be046a43fbd6eec19fdf0cc9d2141b69bc93c879
nikitasnv <nikitasnv@mail.ru>
2021-05-10 11:11:18 +0300

    merge with main branch

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:vue/recommended"
+    ],
+    rules: {
+        'object-curly-spacing': ["warn", "always"]
+    },
+    parserOptions: {
+        "parser": "babel-eslint"
+    }
+}
